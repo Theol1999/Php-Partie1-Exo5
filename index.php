f1bdec3eeabb8cc977ee9770eee1b00439de4093
theoL <theo1007@outlook.fr>
2019-03-13 15:10:39 +0100

    Ajout de l'exo 5

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$int = NULL;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $int = 51;
+        echo "$int";
+       ?>
+    </p>
+  </body>
+</html>
